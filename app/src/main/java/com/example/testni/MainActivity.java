fa57e0601148125383fa3de56f590573ddbc1efa
Hrvoje <pazmu.mostar@gmail.com>
2016-11-11 11:01:37 +0100

    Prvi commit

---
+++ app/src/main/java/com/example/testni/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.testni;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // git config --global user.email ---email--
+        // git config --global user.username ---username---
+        // git config --global user.password ---lozinka---
+         // git remote add origin https://github.com/pazmu/testni.git
+    }
+}
